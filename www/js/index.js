469adec9d85ff8d85b9db19e64e718159888ff6e
Zubbo3 <zubbo3@gmail.com>
2017-12-15 16:31:55 +0100

    Creato layout AggiungiStato Modificato Profilo

--- www/js/index.js
+++ www/js/index.js
@@ -20,6 +20,28 @@ $(document).ready(function(){
                 } else
                     alert("Controlla la connessione a internet!");
             });
+    }); // button
+});
+
+$(document).ready(function(){
+
+    $("#bLogout").click(function(){
+        console.log("div logout");
+        console.log("session id: "+$("#session_id").val());
+
+
+        $.get("https://ewserver.di.unimi.it/mobicomp/geopost/logout",
+            {
+                session: $("#session_id").val()
+            },
+            function(session){
+                console.log("session: " + session);
+                if (session) {
+                    $("#session_id").html(data);
+                    location.href="index.html";
+                } else
+                    alert("Logout non disponibile");
+            });
 
     }); // button
 
